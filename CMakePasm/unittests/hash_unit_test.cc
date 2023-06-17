8dac1d48a334a0299e008109b41a558e1592769c
Paul Baxter <jareth_2005@yahoo.com>
2023-06-17 18:58:52 -0400

    add tests

---
+++ CMakePasm/unittests/hash_unit_test.cc
@@ -0,0 +1,36 @@
+#include <gtest/gtest.h>
+#include "unit_test.h"
+
+
+static void hash_unit_test_method_initialize();
+static void hash_unit_test_method_cleanup();
+
+static void hash_unit_test_method_initialize()
+{
+    initialize();
+}
+
+static void hash_unit_test_method_cleanup()
+{
+    destroy();
+}
+
+TEST(hash_unit_test, hash_test)
+{
+    const auto hash_value = hash_function(__FILE__);
+    EXPECT_NE(0U, hash_value);
+
+    const auto hash_value2 = hash_function(__FILE__);
+    EXPECT_EQ(hash_value, hash_value2);	
+}
+
+TEST(hash_unit_test, hash_pointer_function_test)
+{
+    int dummy = 0x4545;
+    
+    const auto hash_value = hash_pointer_function(&dummy);
+    EXPECT_NE(0U, hash_value);
+
+    const auto hash_value2 = hash_pointer_function(&dummy);
+    EXPECT_EQ(hash_value, hash_value2);	
+}
