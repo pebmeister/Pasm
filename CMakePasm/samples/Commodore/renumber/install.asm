6572b82d50ac8fd5b2971c16bf2f51b17c065817
Paul Baxter <jareth_2005@yahoo.com>
2026-06-25 11:32:14 -0400

    Update install.asm
    let rom perform NEW

--- CMakePasm/samples/Commodore/renumber/install.asm
+++ CMakePasm/samples/Commodore/renumber/install.asm
@@ -20,29 +20,16 @@ INSTALL
         MOVE16 IGONE, IGONE_SV
         MOVE16I IGONE, WEDGE
 
-        lda #0                  ; peform a NEW for basic
-        ldy #NEWBYTES
--
-        sta (TXTTAB),y
-        dey
-        bne -
+		jsr SCRTCH           	; Call the BASIC "NEW" routine
+
 
         ;
         ;   set NXTLN
         ;
         lda TXTTAB              ;   load start of basic
         sta NXTLN               ;   into next line
-        sta STREND
         lda TXTTAB + 1
         sta NXTLN + 1
-        sta STREND + 1
-
-        lda STREND
-        sta VARTAB
-        sta ARYTAB
-        lda STREND + 1
-        sta VARTAB + 1
-        sta ARYTAB + 1
 
         cli
         
